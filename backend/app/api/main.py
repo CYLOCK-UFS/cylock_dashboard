2504a3d719898183c936eaa2f98a66488ba9a7d0
phmnsx <161394779+phmnsx@users.noreply.github.com>
2026-06-17 09:57:56 -0300

    feat: termino de main.py

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Depends
-#Esperado: algo como "LocalSession" para gerar minha sessão
-from model import engine, LocalSession, Noticia
-from sqlalchemy import select
+from fastapi import FastAPI, Depends, HTTPException
+# Esperado: algo como "LocalSession" para gerar minha sessão, 
+# tabelas "Noticia", "Categoria", "Fonte"
+from model import engine, LocalSession, Noticia, Categoria, Fonte
+from datetime import datetime, timedelta, timezone
+from sqlalchemy import select, func
 
 app = FastAPI()
 itemsperpage = 20
@@ -19,41 +21,83 @@ async def list_items(limit: int = 20,
                      p: int = 0, 
                      q: str = None, 
                      categoria: str = None, 
-                     fonte: str = None, 
+                     src: str = None, 
                      db = Depends(get_db)):
     
     stmt = select(Noticia)
-    filters = {
-        "categoria": Noticia.categoria,
-        "fonte": Noticia.fonte,
-    }
 
-    for param, coluna in filters.items():
-        value = locals().get(param)
-        if value is not None:
-            stmt = stmt.where(coluna == value)
+    # join ou relationship?
+    if categoria:
+        stmt = stmt.join(Categoria, Noticia.categoria == Categoria.id)
+        stmt = stmt.where(Categoria.nome == categoria)
+
+    if src:
+        stmt = stmt.join(Fonte, Noticia.fonte == Fonte.id)
+        stmt = stmt.where(Fonte.nome == src)
     
     if q:
         stmt = stmt.where(Noticia.titulo.contains(q))
     
     offset = p * limit
     stmt = stmt.offset(offset).limit(limit)
+    
 
-    noticias = db.execute(stmt).scalars().all()
+    noticias = db.execute(stmt.order_by(Noticia.data.desc())).scalars().all()
     return noticias
 
-
-
-@app.get("/noticias/{id}/")
+@app.get("/noticias/{id}")
 async def read_item(id: int, db = Depends(get_db)):
     stmt = select(Noticia).where(Noticia.id == id)
     noticia = db.execute(stmt).scalars().first()
     if noticia is None:
-        # Opcional: levantar HTTPException
-        return {"erro": "Notícia não encontrada"}
+        raise HTTPException(status_code=404, detail="Noticia nao encontrada.")
+    return noticia
+
+@app.get("/categorias")
+async def list_cat(db = Depends(get_db)):
+    stmt = select(Categoria)
+    categorias = db.execute(stmt).scalars().all()
+    return categorias
+
+@app.get("/fontes")
+async def list_source(db = Depends(get_db)):
+    stmt = select(Fonte)
+    fontes = db.execite(stmt).scalars.all()
+    return fontes
+
+@app.get("/estatisticas")
+async def list_estatisticas(db = Depends(get_db)):
+    stmt_cat = (
+        select(Categoria.nome, func.count(Noticia.id).label("total"))
+        .outerjoin(Noticia, Noticia.categoria_id == Categoria.id)
+        .group_by(Categoria.id)
+    )
+    resultados_cat = db.execute(stmt_cat).all()
+    dict_categoria = {nome: total for nome, total in resultados_cat}
+    stmt_fonte = (
+        select(Fonte.nome, func.count(Noticia.id).label("total"))
+        .outerjoin(Noticia, Noticia.fonte_id == Fonte.id)
+        .group_by(Fonte.id)
+    )
+    resultados_fonte = db.execute(stmt_fonte).all()
+    dict_fonte = {nome: total for nome, total in resultados_fonte}
     return {
-        "id": noticia.id,
-        "titulo": noticia.titulo,
-        "conteudo": noticia.conteudo,
-        "autor": noticia.autor
+        "categoria": dict_categoria,
+        "fonte": dict_fonte
     }
+
+@app.get("/estatisticas/tendencias")
+async def make_tendencies(db = Depends(get_db)):
+    # Funciona?
+    stmt = (
+    select(Categoria.nome, func.count(Noticia.id).label("total"))
+    .join(Noticia, Noticia.categoria_id == Categoria.id)
+    )
+
+    data_limite = datetime.now() - timedelta(days=28)
+    stmt = select(Noticia)
+    stmt = stmt.where(Noticia.data >= data_limite)
+    stmt = stmt.group_by(Categoria.id)
+    stmt = stmt.order_by(func.count(Noticia.id).desc())
+    resultados = db.execute(stmt).all().limit(5)
+    return [{"categoria": nome, "total": total} for nome, total in resultados]
